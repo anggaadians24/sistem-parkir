64a22c544bbbc842564ce71077e5aab22f42c172
anggaadians24 <47051299+anggaadians24@users.noreply.github.com>
2019-05-21 14:28:20 +0700

    angga v2
    v2

--- LED.ino
+++ LED.ino
@@ -1,1 +1,33 @@
-
+void led{
+  {
+digitalWrite(ledPin1, HIGH);
+digitalWrite(ledPin2, LOW);
+digitalWrite(ledPin3, LOW);
+digitalWrite(ledPin4, LOW);
+digitalWrite(ledPin5, LOW);
+delay(1000);
+digitalWrite(ledPin1, LOW);
+digitalWrite(ledPin2, HIGH);
+digitalWrite(ledPin3, LOW);
+digitalWrite(ledPin4, LOW);
+digitalWrite(ledPin5, LOW);
+delay(1000);
+digitalWrite(ledPin1, LOW);
+digitalWrite(ledPin2, LOW);
+digitalWrite(ledPin3, HIGH);
+digitalWrite(ledPin4, LOW);
+digitalWrite(ledPin5, LOW);
+delay(1000);
+digitalWrite(ledPin1, LOW);
+digitalWrite(ledPin2, LOW);
+digitalWrite(ledPin3, LOW);
+digitalWrite(ledPin4, HIGH);
+digitalWrite(ledPin5, LOW);
+delay(1000);
+digitalWrite(ledPin1, LOW);
+digitalWrite(ledPin2, LOW);
+digitalWrite(ledPin3, LOW);
+digitalWrite(ledPin4, LOW);
+digitalWrite(ledPin5, HIGH);
+delay(1000);
+}
--- sketch_may14a.ino
+++ sketch_may14a.ino
@@ -2,13 +2,20 @@
 #include <HMC5883L.h>
 HMC5883L compass;
 
-//====
+//==== sensor
 int obstaclePin = 2;
 int obstaclePin = 3;
 int obstaclePin = 4;
 int obstaclePin = 5;
 int obstaclePin = 6;
-//===================
+//===== LED 
+int ledPin1 = 7;
+int ledPin2 = 8;
+int ledPin3 = 9;
+int ledPin4 = 10;
+int ledpin5 = 11;
+
+//========
 
 #define sensorA  2
 #define sensorB  3
