3c6fa5e7ff9d4b9a34e5b88cb30cbeab22305713
SofrinaMuizzunHakim <32546140+SofrinaMuizzunHakim@users.noreply.github.com>
2019-05-21 14:31:19 +0700

    v2
    sofrina muizzun hakim

---
+++ admin/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php 
+	session_start();
+	include 'cek.php';
+	include 'config.php';
+	?>
+	<title>Aplikasi Parkir Otomatis</title>
+	<link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="../assets/js/jquery-ui/jquery-ui.css">
+	<script type="text/javascript" src="../assets/js/jquery.js"></script>
+	<script type="text/javascript" src="../assets/js/jquery.js"></script>
+	<script type="text/javascript" src="../assets/js/bootstrap.js"></script>
+	<script type="text/javascript" src="../assets/js/jquery-ui/jquery-ui.js"></script>	
+</head>
+<body>
+
+
+	
+
+	
+
+		
---
+++ admin/index.php
@@ -0,0 +1,19 @@
+<?php 
+include 'header.php';
+?>
+
+<?php
+
+?>
+
+<div class="col-md-10">
+	<h3>Selamat datang,<?php echo $_SESSION['uname']  ?></h3>
+    <h3>Aplikasi Parkir Otomatis</h3>
+   
+</div>
+
+
+<?php 
+
+
+?>
