d64a71e62bf3345f71841b2cc4be183e94b564a9
Tegar Arif Dananjaya <tegararif86@gmail.com>
2019-05-28 15:06:20 +0700

    ekstrak assets

--- interace.php
+++ interace.php
@@ -8,7 +8,21 @@
 	
 </head>
 <body>
-	<div class="wrap">
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<a class="navbar-brand" href="#">Navbar</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+				<div class="navbar-nav">
+				<a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+				<a class="nav-item nav-link" href="#">Features</a>
+				<a class="nav-item nav-link" href="#">Pricing</a>
+				<a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+				</div>
+			</div>
+		</nav>
+	<!-- <div class="wrap">
 		<div class="header">			
 			<h1>PARKIR OTOMATIS</h1>
 			<p>sistem parkir mandiri</p>
@@ -36,7 +50,7 @@
 		<div class="footer">
 			about
 		</div>
-	</div>
+	</div> -->
 	<script type="text/javascript" src="assets/js/jquery.js"></script>
 	<script type="text/javascript" src="assets/js/bootstrap.js"></script>
 	<script type="text/javascript" src="assets/js/jquery-ui/jquery-ui.js"></script>
