e6009b22309020ac102f5c508c13c22f905ac8a1
Tegar Arif Dananjaya <tegararif86@gmail.com>
2019-05-28 15:37:19 +0700

    penambahan isi

--- interace.php
+++ interace.php
@@ -2,26 +2,53 @@
 <html>
 <head>
 	<title></title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
-	<link rel="stylesheet" type="text/css" href="assets/js/jquery-ui/jquery-ui.css">
+	<!-- <link rel="stylesheet" type="text/css" href="style.css"> -->
+	<link rel="stylesheet" type="text/css" href="assets/bootstrap4/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="assets/bootstrap4/css/bootstrap.min.css">
+	
+	<!-- <link rel="stylesheet" type="text/css" href="assets/js/jquery-ui/jquery-ui.css"> -->
 	
 </head>
 <body>
-		<nav class="navbar navbar-expand-lg navbar-light bg-light">
-			<a class="navbar-brand" href="#">Navbar</a>
+		<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+			<div class="container">
+			<a class="navbar-brand" href="#">Parkir Otomatis</a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
 			<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
 				<div class="navbar-nav">
 				<a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-				<a class="nav-item nav-link" href="#">Features</a>
-				<a class="nav-item nav-link" href="#">Pricing</a>
-				<a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+				<a class="nav-item nav-link" href="#">Map Order</a>
+				<a class="nav-item nav-link" href="#">About</a>
+				
 				</div>
 			</div>
+			</div>
 		</nav>
+		<div class="container">
+		<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+			<div class="carousel-inner">
+				<div class="carousel-item active">
+				<img src="assets/img/5.png" class="d-block w-100" alt="...">
+				</div>
+				<div class="carousel-item">
+				<img src="assets/img/5.png" class="d-block w-100" alt="...">
+				</div>
+				<div class="carousel-item">
+				<img src="assets/img/5.png" class="d-block w-100" alt="...">
+				</div>
+			</div>
+			<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			</a>
+			</div>
+			</div>
 	<!-- <div class="wrap">
 		<div class="header">			
 			<h1>PARKIR OTOMATIS</h1>
@@ -51,8 +78,9 @@
 			about
 		</div>
 	</div> -->
-	<script type="text/javascript" src="assets/js/jquery.js"></script>
-	<script type="text/javascript" src="assets/js/bootstrap.js"></script>
-	<script type="text/javascript" src="assets/js/jquery-ui/jquery-ui.js"></script>
+	<script type="text/javascript" src="assets/bootstrap4/js/jquery.js"></script>
+	<script type="text/javascript" src="assets/bootstrap4/js/bootstrap.js"></script>
+	<script type="text/javascript" src="assets/bootstrap4/js/jquery-ui/jquery-ui.js"></script>
+	
 </body>
 </html>
