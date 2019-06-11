8947f769f6dc294914c5c5ec6aeab2d2533e6c7c
SofrinaMuizzunHakim <32546140+SofrinaMuizzunHakim@users.noreply.github.com>
2019-06-11 14:39:40 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<!-- <link rel="stylesheet" type="text/css" href="style.css"> -->
+	<link rel="stylesheet" type="text/css" href="assets/bootstrap4/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="assets/bootstrap4/css/bootstrap.min.css">
+	
+	<!-- <link rel="stylesheet" type="text/css" href="assets/js/jquery-ui/jquery-ui.css"> -->
+	
+</head>
+<body>
+		<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+			<div class="container">
+			<a class="navbar-brand" href="#">Parkir Otomatis</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+				<div class="navbar-nav">
+				<a class="nav-item nav-link active" href="interace.php">Home <span class="sr-only">(current)</span></a>
+				<a class="nav-item nav-link" href="#">Map Order</a>
+				<a class="nav-item nav-link" href="about.php">About</a>
+				
+				</div>
+			</div>
+			</div>
+		</nav>
+	<div class="row"></div>
+			<div class="container">
+
+		<ul class="list-group">
+			<li class="list-group-item">APLIKASI PARKIR OTOMATIS</a></li>
+			<li class="list-group-item">BETA VERSION</a></li>
+			<li class="list-group-item">KELOMPOK 5</a></li>
+					
+		</ul>
+	</div>
+</div>
+	<script type="text/javascript" src="assets/bootstrap4/js/jquery.js"></script>
+	<script type="text/javascript" src="assets/bootstrap4/js/bootstrap.js"></script>
+	<script type="text/javascript" src="assets/bootstrap4/js/jquery-ui/jquery-ui.js"></script>
+	
+</body>
+</html>
