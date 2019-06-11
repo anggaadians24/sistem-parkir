40ed2e96b482c076a6bb2624915a3c1ecbe36c4d
SofrinaMuizzunHakim <32546140+SofrinaMuizzunHakim@users.noreply.github.com>
2019-06-11 14:08:45 +0700

    v4

--- admin/index.php
+++ admin/index.php
@@ -1,16 +1,11 @@
 <?php 
-include 'header.php';
+include 'interace.php';
 ?>
 
 <?php
 
 ?>
 
-<div class="col-md-10">
-	<h3>Selamat datang,<?php echo $_SESSION['uname']  ?></h3>
-    <h3>Aplikasi Parkir Otomatis</h3>
-   
-</div>
 
 
 <?php 
