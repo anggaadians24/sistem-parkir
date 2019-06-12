33f30aecb818a774982e6ac9ed80512d8065515c
ranggawhedi <50511750+ranggawhedi@users.noreply.github.com>
2019-06-12 22:00:21 +0700

    Add files via upload

---
+++ parkir_mobil.sql
@@ -0,0 +1,44 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 12, 2019 at 04:57 PM
+-- Server version: 10.1.34-MariaDB
+-- PHP Version: 7.2.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sistem_parkir`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `parkir mobil`
+--
+
+CREATE TABLE `parkir mobil` (
+  `id_parkir` varchar(10) NOT NULL,
+  `no_id` varchar(10) NOT NULL,
+  `slot` varchar(5) NOT NULL,
+  `tanggal` date NOT NULL,
+  `jam keluar` int(2) NOT NULL,
+  `biaya` int(8) NOT NULL,
+  `riwayat` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
